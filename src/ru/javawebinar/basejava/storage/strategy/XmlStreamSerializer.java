5ea619500103622f8d5236b613da818aebe03f31
Space Cowboy <spacecowboy790@gmail.com>
2021-05-25 00:01:14 +0700

    Lesson 10 HM 25/05/21

---
+++ src/ru/javawebinar/basejava/storage/strategy/XmlStreamSerializer.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.basejava.storage.strategy;
+
+import ru.javawebinar.basejava.model.*;
+import ru.javawebinar.basejava.util.XmlParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class XmlStreamSerializer implements StreamSerializer {
+
+    private XmlParser xmlParser;
+
+    public XmlStreamSerializer() {
+        xmlParser = new XmlParser(
+                Resume.class, Organization.class, Link.class,
+                OrganizationSection.class, TextSection.class, ListSection.class, Organization.Position.class);
+    }
+
+    @Override
+    public void writeResume(OutputStream os, Resume resume) throws IOException {
+        try (Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+            xmlParser.marshall(resume, w);
+        }
+    }
+
+    @Override
+    public Resume readResume(InputStream is) throws IOException {
+        try (Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            return xmlParser.unmarshall(r);
+        }
+    }
+}
