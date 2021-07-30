e7652430323bc14ff24a319a554be1dc65e71e10
Space Cowboy <spacecowboy790@gmail.com>
2021-07-30 22:55:04 +0700

    Lesson 11 HM 30/07/21

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -18,11 +18,11 @@ public class MainDeadLock {
 
     public static void main(String[] args) {
         MainDeadLock deadlock = new MainDeadLock();
-        new Thread(() -> deadlock.makeDeadLock(deadlock.getLock1(), deadlock.getLock2())).start();
-        new Thread(() -> deadlock.makeDeadLock(deadlock.getLock2(), deadlock.getLock1())).start();
+        new Thread(() -> makeDeadLock(deadlock.getLock1(), deadlock.getLock2())).start();
+        new Thread(() -> makeDeadLock(deadlock.getLock2(), deadlock.getLock1())).start();
     }
 
-    public void makeDeadLock(Lock lock1, Lock lock2) {
+    public static void makeDeadLock(Lock lock1, Lock lock2) {
         lock1.lock();
         System.out.println(lock1 + " — захвачен");
         lock2.lock();
