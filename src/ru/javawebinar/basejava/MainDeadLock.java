c2d389b39c4b4617a1fa644a9447031fce23e481
Space Cowboy <spacecowboy790@gmail.com>
2021-08-03 22:28:13 +0700

    Lesson 11 HM 03/08/21

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -1,45 +1,48 @@
 package ru.javawebinar.basejava;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class MainDeadLock {
 
-    private Lock lock1 = new ReentrantLock(true);
-    private Lock lock2 = new ReentrantLock(true);
-
-    public Lock getLock1() {
-        return lock1;
-    }
-
-    public Lock getLock2() {
-        return lock2;
-    }
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) {
-        MainDeadLock deadlock = new MainDeadLock();
-        new Thread(() -> makeDeadLock(deadlock.getLock1(), deadlock.getLock2())).start();
-        new Thread(() -> makeDeadLock(deadlock.getLock2(), deadlock.getLock1())).start();
+        new Thread(() -> makeDeadLock(LOCK1, LOCK2)).start();
+        new Thread(() -> makeDeadLock(LOCK2, LOCK1)).start();
     }
 
-    public static void makeDeadLock(Lock lock1, Lock lock2) {
-        lock1.lock();
-        System.out.println("Поток " + Thread.currentThread().getName() + " захватил ресурс " + lock1);
-        lock2.lock();
-        System.out.println("Поток " + Thread.currentThread().getName() + " захватил ресурс " + lock2);
+    public static void makeDeadLock(Object lock1, Object lock2) {
+        synchronized (lock1) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Поток " + threadName + " захватил ресурс " + lock1);
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Поток " + threadName + " ожидает захвата ресурса " + lock2);
+
+            synchronized (lock2) {
+                System.out.println("Поток " + threadName + " захватил ресурс " + lock2);
+            }
         }
 
-        lock2.lock();
-        System.out.println("Поток " + Thread.currentThread().getName() + " захватил ресурс " + lock2);
-        lock1.lock();
-        System.out.println("Поток " + Thread.currentThread().getName() + " захватил ресурс " + lock1);
+        synchronized (lock2) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Поток " + threadName + " захватил ресурс " + lock2);
 
-        lock1.unlock();
-        lock2.unlock();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Поток " + threadName + " ожидает захвата ресурса " + lock1);
+
+            synchronized (lock1) {
+                System.out.println("Поток " + threadName + " захватил ресурс " + lock1);
+            }
+        }
     }
 }
