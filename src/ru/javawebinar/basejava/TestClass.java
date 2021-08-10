bf5fc4ee3f37fee3683c4c01b8ffe69de930bc24
Space Cowboy <spacecowboy790@gmail.com>
2021-08-11 00:37:32 +0700

    Lesson 13 HM 10/08/21

--- src/ru/javawebinar/basejava/TestClass.java
+++ src/ru/javawebinar/basejava/TestClass.java
@@ -2,6 +2,6 @@ package ru.javawebinar.basejava;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world ");
     }
 }
